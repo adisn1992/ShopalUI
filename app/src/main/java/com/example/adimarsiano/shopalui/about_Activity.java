4d6528e32fb2d21a8e2129c533ba83eba0782d75
adi <adisityv@gmail.com>
2018-10-12 13:03:39 +0300

    adding new activity about

--- app/src/main/java/com/example/adimarsiano/shopalui/about_Activity.java
+++ app/src/main/java/com/example/adimarsiano/shopalui/about_Activity.java
@@ -1,9 +1,18 @@
 package com.example.adimarsiano.shopalui;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class about_Activity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_account);
+    }
 
     @Override
     public void onClick(View v) {
